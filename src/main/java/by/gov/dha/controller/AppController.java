13d787f2255ffdb3b6fd759a720defe9d95fab98
Алексей Мелещеня <alexey.meleshchenya@compit.by>
2018-09-12 13:29:36 +0300

    test

--- src/main/java/by/gov/dha/controller/AppController.java
+++ src/main/java/by/gov/dha/controller/AppController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/")
 public class AppController {
 
+    
     @Autowired
     private DocService docService;
 
